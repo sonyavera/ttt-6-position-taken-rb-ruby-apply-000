0d59cd91782eeaf08f10782b5684fce55a87c6fb
Sonya Gould <sonyavgould@gmail.com>
2020-05-21 03:08:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if array[board] == " "
-    false
-  elsif array[board] == ""
+  if board[index] == " " || board[index] == ""
     false
   else
     true
